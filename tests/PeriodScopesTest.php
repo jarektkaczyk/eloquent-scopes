60b2bc40f48cee5f7c4da3054a1f60374f419e25
Jarek Tkaczyk <jarek@softonsofa.com>
2015-09-16 17:45:14 +0200

    adjust tests

--- tests/PeriodScopesTest.php
+++ tests/PeriodScopesTest.php
@@ -23,261 +23,277 @@ class PeriodScopesTest extends \PHPUnit_Framework_TestCase
     /** @test */
     public function lastPeriodsWithCurrent()
     {
-        $query = ModelStub::query()->periods('year', -2, true);
+        $query = $this->getModel()->newQuery()->periods('year', -2, true);
 
         $range = [
             Carbon::now()->subYears(2)->startOfYear(),
             Carbon::now()->endOfYear(),
         ];
-        $this->assertEquals("select * from `users` where `created_at` between ? and ?", $query->toSql());
+        $this->assertEquals('select * from "users" where "created_at" between ? and ?', $query->toSql());
         $this->assertEquals($range, $query->getBindings());
     }
 
     /** @test */
     public function lastPeriods()
     {
-        $query = ModelStub::periods('year', -2);
+        $query = $this->getModel()->periods('year', -2);
 
         $range = [
             Carbon::now()->subYears(2)->startOfYear(),
             Carbon::now()->subYear()->endOfYear()
         ];
-        $this->assertEquals("select * from `users` where `created_at` between ? and ?", $query->toSql());
+        $this->assertEquals('select * from "users" where "created_at" between ? and ?', $query->toSql());
         $this->assertEquals($range, $query->getBindings());
     }
 
     /** @test */
     public function nextPeriodsWithCurrent()
     {
-        $query = ModelStub::periods('year', 3, null, true);
+        $query = $this->getModel()->periods('year', 3, null, true);
 
         $range = [
             Carbon::now()->startOfYear(),
             Carbon::now()->addYears(3)->endOfYear()
         ];
-        $this->assertEquals("select * from `users` where `created_at` between ? and ?", $query->toSql());
+        $this->assertEquals('select * from "users" where "created_at" between ? and ?', $query->toSql());
         $this->assertEquals($range, $query->getBindings());
     }
 
     /** @test */
     public function nextPeriods()
     {
-        $query = ModelStub::periods('year', 3);
+        $query = $this->getModel()->periods('year', 3);
 
         $range = [
             Carbon::now()->addYear()->startOfYear(),
             Carbon::now()->addYears(3)->endOfYear()
         ];
-        $this->assertEquals("select * from `users` where `created_at` between ? and ?", $query->toSql());
+        $this->assertEquals('select * from "users" where "created_at" between ? and ?', $query->toSql());
         $this->assertEquals($range, $query->getBindings());
     }
 
     /** @test */
     public function thisYearWithColumnProvided()
     {
-        $query = ModelStub::thisYear('another_column');
+        $query = $this->getModel()->thisYear('another_column');
 
         $range = [
             Carbon::now()->startOfYear(),
             Carbon::now()->endOfYear()
         ];
-        $this->assertEquals("select * from `users` where `another_column` between ? and ?", $query->toSql());
+        $this->assertEquals('select * from "users" where "another_column" between ? and ?', $query->toSql());
         $this->assertEquals($range, $query->getBindings());
     }
 
     /** @test */
     public function thisYearWithColumnSpecified()
     {
-        $query = ModelStubWithColumn::thisYear();
+        $query = $this->getModel('ModelStubWithColumn')->thisYear();
 
         $range = [
             Carbon::now()->startOfYear(),
             Carbon::now()->endOfYear()
         ];
-        $this->assertEquals("select * from `users` where `custom_timestamp` between ? and ?", $query->toSql());
+        $this->assertEquals('select * from "users" where "custom_timestamp" between ? and ?', $query->toSql());
         $this->assertEquals($range, $query->getBindings());
     }
 
     /** @test */
     public function nextYear()
     {
-        $query = ModelStub::nextYear();
+        $query = $this->getModel()->nextYear();
 
         $range = [
             Carbon::parse('+1 year')->startOfYear(),
             Carbon::parse('+1 year')->endOfYear()
         ];
-        $this->assertEquals("select * from `users` where `created_at` between ? and ?", $query->toSql());
+        $this->assertEquals('select * from "users" where "created_at" between ? and ?', $query->toSql());
         $this->assertEquals($range, $query->getBindings());
     }
 
     /** @test */
     public function nextMonth()
     {
-        $query = ModelStub::nextMonth();
+        $query = $this->getModel()->nextMonth();
 
         $range = [
             Carbon::parse('+1 month')->startOfMonth(),
             Carbon::parse('+1 month')->endOfMonth()
         ];
-        $this->assertEquals("select * from `users` where `created_at` between ? and ?", $query->toSql());
+        $this->assertEquals('select * from "users" where "created_at" between ? and ?', $query->toSql());
         $this->assertEquals($range, $query->getBindings());
     }
 
     /** @test */
     public function tomorrow()
     {
-        $query = ModelStub::tomorrow();
+        $query = $this->getModel()->tomorrow();
 
         $range = [
             Carbon::tomorrow(),
             Carbon::tomorrow()->endOfDay()
         ];
-        $this->assertEquals("select * from `users` where `created_at` between ? and ?", $query->toSql());
+        $this->assertEquals('select * from "users" where "created_at" between ? and ?', $query->toSql());
         $this->assertEquals($range, $query->getBindings());
     }
 
     /** @test */
     public function nextHour()
     {
-        $query = ModelStub::nextHour();
+        $query = $this->getModel()->nextHour();
 
         $range = [
             Carbon::parse('+1 hour')->minute(0)->second(0),
             Carbon::parse('+1 hour')->minute(59)->second(59)
         ];
-        $this->assertEquals("select * from `users` where `created_at` between ? and ?", $query->toSql());
+        $this->assertEquals('select * from "users" where "created_at" between ? and ?', $query->toSql());
         $this->assertEquals($range, $query->getBindings());
     }
 
     /** @test */
     public function nextMinute()
     {
-        $query = ModelStub::nextMinute();
+        $query = $this->getModel()->nextMinute();
 
         $range = [
             Carbon::parse('+1 minute')->second(0),
             Carbon::parse('+1 minute')->second(59)
         ];
-        $this->assertEquals("select * from `users` where `created_at` between ? and ?", $query->toSql());
+        $this->assertEquals('select * from "users" where "created_at" between ? and ?', $query->toSql());
         $this->assertEquals($range, $query->getBindings());
     }
 
     /** @test */
     public function thisYear()
     {
-        $query = ModelStub::thisYear();
+        $query = $this->getModel()->thisYear();
 
         $range = [
             Carbon::now()->startOfYear(),
             Carbon::now()->endOfYear()
         ];
-        $this->assertEquals("select * from `users` where `created_at` between ? and ?", $query->toSql());
+        $this->assertEquals('select * from "users" where "created_at" between ? and ?', $query->toSql());
         $this->assertEquals($range, $query->getBindings());
     }
 
     /** @test */
     public function thisMonth()
     {
-        $query = ModelStub::thisMonth();
+        $query = $this->getModel()->thisMonth();
 
         $range = [
             Carbon::now()->startOfMonth(),
             Carbon::now()->endOfMonth()
         ];
-        $this->assertEquals("select * from `users` where `created_at` between ? and ?", $query->toSql());
+        $this->assertEquals('select * from "users" where "created_at" between ? and ?', $query->toSql());
         $this->assertEquals($range, $query->getBindings());
     }
 
     /** @test */
     public function today()
     {
-        $query = ModelStub::today();
+        $query = $this->getModel()->today();
 
         $range = [
             Carbon::today(),
             Carbon::today()->endOfDay()
         ];
-        $this->assertEquals("select * from `users` where `created_at` between ? and ?", $query->toSql());
+        $this->assertEquals('select * from "users" where "created_at" between ? and ?', $query->toSql());
         $this->assertEquals($range, $query->getBindings());
     }
 
     /** @test */
     public function thisHour()
     {
-        $query = ModelStub::thisHour();
+        $query = $this->getModel()->thisHour();
 
         $range = [
             Carbon::now()->minute(0)->second(0),
             Carbon::now()->minute(59)->second(59)
         ];
-        $this->assertEquals("select * from `users` where `created_at` between ? and ?", $query->toSql());
+        $this->assertEquals('select * from "users" where "created_at" between ? and ?', $query->toSql());
         $this->assertEquals($range, $query->getBindings());
     }
 
     /** @test */
     public function lastYear()
     {
-        $query = ModelStub::lastYear();
+        $query = $this->getModel()->lastYear();
 
         $range = [
             Carbon::parse('-1 year')->startOfYear(),
             Carbon::parse('-1 year')->endOfYear()
         ];
-        $this->assertEquals("select * from `users` where `created_at` between ? and ?", $query->toSql());
+        $this->assertEquals('select * from "users" where "created_at" between ? and ?', $query->toSql());
         $this->assertEquals($range, $query->getBindings());
     }
 
     /** @test */
     public function lastMonth()
     {
-        $query = ModelStub::lastMonth();
+        $query = $this->getModel()->lastMonth();
 
         $range = [
             Carbon::parse('-1 month')->startOfMonth(),
             Carbon::parse('-1 month')->endOfMonth()
         ];
-        $this->assertEquals("select * from `users` where `created_at` between ? and ?", $query->toSql());
+        $this->assertEquals('select * from "users" where "created_at" between ? and ?', $query->toSql());
         $this->assertEquals($range, $query->getBindings());
     }
 
     /** @test */
     public function yesterday()
     {
-        $query = ModelStub::yesterday();
+        $query = $this->getModel()->yesterday();
 
         $range = [
             Carbon::yesterday(),
             Carbon::yesterday()->endOfDay()
         ];
-        $this->assertEquals("select * from `users` where `created_at` between ? and ?", $query->toSql());
+        $this->assertEquals('select * from "users" where "created_at" between ? and ?', $query->toSql());
         $this->assertEquals($range, $query->getBindings());
     }
 
     /** @test */
     public function lastHour()
     {
-        $query = ModelStub::lastHour();
+        $query = $this->getModel()->lastHour();
 
         $range = [
             Carbon::parse('-1 hour')->minute(0)->second(0),
             Carbon::parse('-1 hour')->minute(59)->second(59)
         ];
-        $this->assertEquals("select * from `users` where `created_at` between ? and ?", $query->toSql());
+        $this->assertEquals('select * from "users" where "created_at" between ? and ?', $query->toSql());
         $this->assertEquals($range, $query->getBindings());
     }
 
     /** @test */
     public function lastMinute()
     {
-        $query = ModelStub::lastMinute();
+        $query = $this->getModel()->lastMinute();
 
         $range = [
             Carbon::parse('-1 minute')->second(0),
             Carbon::parse('-1 minute')->second(59)
         ];
-        $this->assertEquals("select * from `users` where `created_at` between ? and ?", $query->toSql());
+        $this->assertEquals('select * from "users" where "created_at" between ? and ?', $query->toSql());
         $this->assertEquals($range, $query->getBindings());
+    }
+
+    public function getModel($stub = 'ModelStub')
+    {
+        $stub = 'Sofa\EloquentScopes\Tests\\'.$stub;
+        $model = new $stub;
+        $grammarClass = 'Illuminate\Database\Query\Grammars\SQLiteGrammar';
+        $processorClass = 'Illuminate\Database\Query\Processors\SQLiteProcessor';
+        $grammar = new $grammarClass;
+        $processor = new $processorClass;
+        $schema = m::mock('StdClass');
+        $connection = m::mock('Illuminate\Database\ConnectionInterface', ['getQueryGrammar' => $grammar, 'getPostProcessor' => $processor]);
+        $connection->shouldReceive('getSchemaBuilder')->andReturn($schema);
+        $resolver = m::mock('Illuminate\Database\ConnectionResolverInterface', ['connection' => $connection]);
+        $stub::setConnectionResolver($resolver);
+        return $model;
     }
 }
 
